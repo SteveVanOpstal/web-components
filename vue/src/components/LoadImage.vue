60f9770bcc5322d00fffa363a2f59c325d09b3be
SteveVanOpstal <stevevanopstal@gmail.com>
2019-02-26 22:47:18 +0100

    add styling to vue

--- vue/src/components/LoadImage.vue
+++ vue/src/components/LoadImage.vue
@@ -1,23 +1,41 @@
 <template>
-  <img class="thumb" v-bind:class="{ready: ready}" src="{{srcThumb}}"/>
-  <img [src]="{{src}}" @load="load"/>
+  <div>
+    <img class="thumb" v-bind:class="{ready: ready}" src="{{srcThumb}}">
+    <img src="{{src}}" @load="load">
+  </div>
 </template>
 
+<style lang="scss" scoped>
+img {
+  width: 400px;
+  height: 300px;
+
+  &.thumb {
+    position: absolute;
+    opacity: 1;
+    transition: opacity 0.5s;
+
+    &.ready {
+      opacity: 0;
+    }
+  }
+}
+</style>
+
 <script>
-import Vue from 'vue';
-import Component from 'vue-class-component';
+import Vue from "vue";
+import Component from "vue-class-component";
 
 @Component({
   props: {
     src: String,
     srcThumb: String
-  },
-  template: ``
+  }
 })
 export default class LoadImageComponent extends Vue {
   ready = false;
 
-  load(): void {
+  load() {
     this.ready = true;
   }
 }
